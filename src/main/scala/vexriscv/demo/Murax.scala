1ff74d6ccbc057d2a95089e5964064a3cef3bfe5
wuxx <447830120@qq.com>
2020-05-05 21:33:52 -0700

    update

--- src/main/scala/vexriscv/demo/Murax.scala
+++ src/main/scala/vexriscv/demo/Murax.scala
@@ -468,7 +468,7 @@ object MuraxDhrystoneReadyMulDivStatic{
 //Will blink led and echo UART RX to UART TX   (in the verilator sim, type some text and press enter to send UART frame to the Murax RX pin)
 object MuraxWithRamInit{
   def main(args: Array[String]) {
-    SpinalVerilog(Murax(MuraxConfig.default.copy(onChipRamSize = 4 kB, onChipRamHexFile = "src/main/ressource/hex/muraxDemo.hex")))
+    SpinalVerilog(Murax(MuraxConfig.default.copy(onChipRamSize = 8 kB, onChipRamHexFile = "src/main/ressource/hex/muraxDemo.hex")))
   }
 }
 
